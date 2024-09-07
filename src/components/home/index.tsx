6113302786cfd58782954a56dd61238f968ae0ce
Andrey Yurlov <43323844+snowdamiz@users.noreply.github.com>
2024-09-07 00:29:09 -0500

    feat: add carousel

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,7 +1,15 @@
-import useHelpers from '../../hooks/useHelpers'
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import useHelpers from '../../hooks/useHelpers.ts'
 import './Home.sass'
 
-function Home({ refs }) {
+function Home({ refs }: any) {
   const { navigate } = useHelpers()
 
   const handleClick = (e, section) => {
@@ -20,7 +28,28 @@ function Home({ refs }) {
             Get a Quote
           </button>
         </div>
+
       </div>
+      <Carousel
+        opts={{ align: "center" }}
+        className="w-full max-w-xs"
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/5">
+              <div className="p-1">
+                <Card className="border-0 shadow-none">
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-3xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   )
 }
