8ac4a8c573a298467b9c7f1f2f1f597cfe90d392
Andrey Yurlov <43323844+snowdamiz@users.noreply.github.com>
2024-09-10 14:34:29 -0500

    feat: contact

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,13 +1,23 @@
 
-import services from '@/assets/services.svg'
+import contact from '@/assets/contact.svg'
 import sectionOne from '@/assets/section-1.svg'
 import SectionHeader from '@/components/section-header'
 import Layout from '@/components/layout'
 import './Contact.sass'
 
 export default function Contact() {
-  const left = <SectionHeader img={sectionOne} title="Contact" />
-  const right = <img src={services} alt="Contact" className="section-bg" />
+  const left = <>
+    <SectionHeader img={sectionOne} title="Contact" />
+    <form className='contact-form'>
+      <input type="text" placeholder="Name" />
+      <input type="email" placeholder="Email" />
+      <input type="tel" placeholder="Phone" />
+      <textarea placeholder="Message" />
+      <button>Send</button>
+    </form>
+  </>
+
+  const right = <img src={contact} alt="Contact" className="section-bg" />
 
   return (
     <Layout
