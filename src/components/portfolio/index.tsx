c5803fa28e637730748d824af982e1da06fe6f8b
Andrey Yurlov <43323844+snowdamiz@users.noreply.github.com>
2024-09-10 14:04:35 -0500

    feat: portfolio

--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -1,18 +1,48 @@
 
-import services from '@/assets/services.svg'
+import { useState } from 'react'
+
 import sectionOne from '@/assets/section-1.svg'
 import SectionHeader from '@/components/section-header'
-import Layout from '@/components/layout'
+import Img1 from '@/assets/images/1.jpeg'
+import Img2 from '@/assets/images/2.jpeg'
+import Img3 from '@/assets/images/3.jpeg'
+import Img4 from '@/assets/images/4.jpeg'
+import Img5 from '@/assets/images/5.jpeg'
+import Img6 from '@/assets/images/6.jpeg'
+import Img7 from '@/assets/images/7.jpeg'
+import Img8 from '@/assets/images/8.jpeg'
+import Img9 from '@/assets/images/9.jpeg'
+import Img10 from '@/assets/images/10.jpeg'
+import Img11 from '@/assets/images/11.jpeg'
+import Img12 from '@/assets/images/12.jpeg'
+import Img13 from '@/assets/images/13.jpeg'
 import './Portfolio.sass'
 
 export default function Portfolio() {
-  const left = <SectionHeader img={sectionOne} title="Portfolio" />
-  const right = <img src={services} alt="Portfolio" className="section-bg" />
+  const [selectedImg, setSelectedImg] = useState(0)
+  const images = [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10, Img11, Img12, Img13]
 
   return (
-    <Layout
-      left={left}
-      right={right}
-    /> 
+    <div className="portfolio-container">
+      <div className='portfolio-inner'>
+        <div className='portfolio-img-container'>
+          <SectionHeader img={sectionOne} title="Portfolio" />
+          <div className='portfolio-img-wrapper'>
+            <img src={images[selectedImg]} alt="Image" className="portfolio-img-selected" />
+          </div>
+        </div>
+        <div className='portfolio-img-gallery'>
+          {images.map((img, index) => (
+            <img
+              src={img}
+              alt="Gallery Image"
+              className="portfolio-img"
+              key={index}
+              onClick={setSelectedImg.bind(null, index)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
